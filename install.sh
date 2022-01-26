483365d5622c49560a77c706e7e75dc7eb1793b6
miningpirate <78247497+miningpirate@users.noreply.github.com>
2022-01-26 19:31:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -156,7 +156,10 @@
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
-    
+    # PHP 7
+echo -e " Installing Ondrej PHP PPA...$COL_RESET"
+if [ ! -f /etc/apt/sources.list.d/ondrej-php-bionic.list ]; then
+hide_output sudo add-apt-repository -y ppa:ondrej/php
     # Installing Installing php7.3
     echo
     echo
@@ -171,17 +174,36 @@
     hide_output sudo apt -y update
 
     if [[ ("$DISTRO" == "16") ]]; then
-    apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
-    php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
-    php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring
-    #hide_output sudo phpenmod mcrypt
-    #hide_output sudo phpenmod mbstring
-    else
-    apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
-    php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
-    php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring \
-    libpsl-dev libnghttp2-dev
-    fi
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
+php-pear php-auth-sasl mcrypt imagemagick libruby \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
+fail2ban ntpdate python3 python3-dev python3-pip \
+curl git sudo coreutils pollinate unzip unattended-upgrades cron \
+pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
+libkrb5-dev libldap2-dev libidn11-dev gnutls-dev librtmp-dev \
+build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainutils libssl-dev \
+automake cmake gnupg2 ca-certificates lsb-release nginx certbot libsodium-dev \
+libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev
+else
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
+php-pear php-auth-sasl mcrypt imagemagick libruby \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
+fail2ban ntpdate python3 python3-dev python3-pip \
+curl git sudo coreutils pollinate unzip unattended-upgrades cron \
+pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
+libkrb5-dev libldap2-dev libidn11-dev gnutls-dev librtmp-dev \
+build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainutils libssl-dev \
+libpsl-dev libnghttp2-dev automake cmake gnupg2 ca-certificates lsb-release nginx certbot libsodium-dev \
+libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev
+fi
+
+    
     sleep 5
     hide_output sudo systemctl start php7.3-fpm
     sudo systemctl status php7.3-fpm | sed -n "1,3p"
